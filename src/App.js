91e18f06a8b9fba18c5769e4769ab7dfcac40443
Filat Astakhov <astakhovfilat@gmail.com>
2019-01-23 13:58:35 +0500

    Добавлены компоненты hero, navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Hero from '../src/components/Hero';
+import Navigation from '../src/components/Navigation';
 
 class App extends Component {
+
   render() {
+
+    let links = [ { href: "#music" , title: "Музыка"},
+                  { href: "#books" , title: "Книги"},
+                  { href: "#movies" , title: "Фильмы"},
+                  { href: "#travel" , title: "Путешествия"},
+                  { href: "#chgk" , title: "ЧГК"}]
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Hero title="TOP2018" bold="bold" />
+        <Navigation links = {links} />
+        <Hero title="Музыка" size="medium" color="danger" bold="bold" anchor="music" />
+        <Hero title="Книги" size="medium" color="warning" bold="bold" anchor="books" />
+        <Hero title="Фильмы" size="medium" color="dark" bold="bold" anchor="movies" />
+        <Hero title="Путешествия" size="medium" color="primary" bold="bold" anchor="travel" />
+        <Hero title="ЧГК" size="medium" color="info" bold="bold" anchor="chgk" />
+      </>
     );
   }
 }
