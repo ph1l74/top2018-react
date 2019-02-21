97b8781408f212e7dc46007d9f022c7bdc92dcb3
Filat Astakhov <astakhovfilat@gmail.com>
2019-02-21 11:45:49 +0500

    Updates

---
+++ precache-manifest.509c232ea287c20a4943f2ab1c552f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f1813c0905c73f2587c",
+    "url": "/top2018/static/js/runtime~main.4f1813c0.js"
+  },
+  {
+    "revision": "c759daeaa42f5e2c87b5",
+    "url": "/top2018/static/js/main.c759daea.chunk.js"
+  },
+  {
+    "revision": "79c1f8c91de64aa70b8a",
+    "url": "/top2018/static/js/1.79c1f8c9.chunk.js"
+  },
+  {
+    "revision": "c759daeaa42f5e2c87b5",
+    "url": "/top2018/static/css/main.6fcd04c0.chunk.css"
+  },
+  {
+    "revision": "960a8649a19428616c6a78941e64c541",
+    "url": "/top2018/index.html"
+  }
+];
